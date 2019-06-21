57646ea26900db58d76d56b1c7612cbc6957e9fd
emilie80 <jeremy.brean@sysid.fr>
2019-06-21 14:20:06 +0200

    Toto

---
+++ tasks.py
@@ -0,0 +1,20 @@
+# tasks.py
+from celery import Celery
+from app import create_app
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(create_app())
